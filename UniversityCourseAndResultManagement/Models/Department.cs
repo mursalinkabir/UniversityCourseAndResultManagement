c1e29ebf6ad0311345e0e5d89647e79ef35945ed
Mursalin Kabir <mail2mursalin@gmail.com>
2016-01-17 19:04:22 +0600

    Comment Test Korlam

--- UniversityCourseAndResultManagement/Models/Department.cs
+++ UniversityCourseAndResultManagement/Models/Department.cs
@@ -7,6 +7,7 @@ namespace UniversityCourseAndResultManagement.Models
 {
     public class Department
     {
+        //testing git
         public int Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
