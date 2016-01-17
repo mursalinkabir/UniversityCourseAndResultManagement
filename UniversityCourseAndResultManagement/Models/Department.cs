4c9786f41515f7678f48a8d3a842ac5bcd9945a2
siam-abdulllah <abdulllahsiam@gmail.com>
2016-01-17 19:01:50 +0600

    Id Add korlam

--- UniversityCourseAndResultManagement/Models/Department.cs
+++ UniversityCourseAndResultManagement/Models/Department.cs
@@ -7,6 +7,7 @@ namespace UniversityCourseAndResultManagement.Models
 {
     public class Department
     {
+        public int Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
     }
