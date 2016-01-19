d0ceed4b9de37d1dc768b1ad4a9943bac1c07234
Mursalin Kabir <mail2mursalin@gmail.com>
2016-01-19 14:30:00 +0600

    Teacher Model Added

---
+++ UniversityCourseAndResultManagement/Models/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityCourseAndResultManagement.Models
+{
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string ContactNo { get; set; }
+        public int DesignationId { get; set; }
+        public string DepartmentId { get; set; }
+        public int Credit { get; set; }
+        public string CourseId { get; set; }
+    }
+}
