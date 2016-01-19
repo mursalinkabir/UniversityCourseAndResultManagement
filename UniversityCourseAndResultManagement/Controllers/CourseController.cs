efe5592bcff65a7dae69daab16a0d93262403ac1
siam-abdulllah <abdulllahsiam@gmail.com>
2016-01-19 21:05:22 +0600

    save course done

---
+++ UniversityCourseAndResultManagement/Controllers/CourseController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversityCourseAndResultManagement.Core.BLL;
+using UniversityCourseAndResultManagement.Models;
+
+namespace UniversityCourseAndResultManagement.Controllers
+{
+    
+    public class CourseController : Controller
+    {
+        DepartmentManager departmentManager=new DepartmentManager();
+        SemesterManager semesterManager=new SemesterManager();
+        CourseManager courseManager=new CourseManager();
+        public ActionResult SaveCourse()
+        {
+
+
+            List<Department> departments = departmentManager.GetAllDepartments();
+            ViewBag.Departments = departments;
+            List<Semester> semesters = semesterManager.GetAllSemester();
+            ViewBag.Semesters = semesters;
+            return View();
+            
+        }
+        [HttpPost]
+        public ActionResult SaveCourse(Course course)
+        { 
+
+
+            List<Department> departments = departmentManager.GetAllDepartments();
+            ViewBag.Departments = departments;
+            List<Semester> semesters = semesterManager.GetAllSemester();
+            ViewBag.Semesters = semesters;
+            ViewBag.Message = courseManager.Save(course);
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
