5e1428582028e0fd2e0d27c8de45ad9f6313ff1e
siam-abdulllah <abdulllahsiam@gmail.com>
2016-01-19 10:30:28 +0600

    course model

---
+++ UniversityCourseAndResultManagement/Core/Gateway/CourseGateway.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityCourseAndResultManagement.Core.Gateway
+{
+    public class CourseGateway
+    {
+    }
+}
