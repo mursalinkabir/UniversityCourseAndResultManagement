2d2f55e789550feaacb21da09d3782785a4ee3fc
Mursalin Kabir <mail2mursalin@gmail.com>
2016-01-22 22:25:20 +0600

    Erro Fixed with Course View

--- UniversityCourseAndResultManagement/Core/Gateway/CourseGateway.cs
+++ UniversityCourseAndResultManagement/Core/Gateway/CourseGateway.cs
@@ -75,38 +75,38 @@ namespace UniversityCourseAndResultManagement.Core.Gateway
         //change by sayed
         public List<Course> GetAllCoursesByDepartment(int deptId)
         {
-            connection.ConnectionString = connectionString;
+            //connection.ConnectionString = connectionString;
 
-            string query = "SELECT * FROM CourseStatics WHERE (DepartmentId=@DepartmentId)";
-            SqlCommand command = new SqlCommand(query, connection);
+            //string query = "SELECT * FROM CourseStatics WHERE (DepartmentId=@DepartmentId)";
+            //SqlCommand command = new SqlCommand(query, connection);
 
-            command.Parameters.Add("DepartmentId", SqlDbType.Int);
-            command.Parameters["DepartmentId"].Value = deptId;
-            connection.Open();
-            SqlDataReader reader = command.ExecuteReader();
-            List<Course> courselist = new List<Course>();
-            while (reader.Read())
-            {
-                Course course = new Course();
-                course.Code = reader["Code"].ToString();
-                course.Name = reader["CourseName"].ToString();
-                course.SemesterName = reader["SemesterName"].ToString();
-                if (!reader["TeacherName"].Equals(System.DBNull.Value))
-                {
-                    course.TeacherName = reader["TeacherName"].ToString();
-                }
-                else
-                {
-                    course.TeacherName = "Yet not assigned";
-                }
+            //command.Parameters.Add("DepartmentId", SqlDbType.Int);
+            //command.Parameters["DepartmentId"].Value = deptId;
+            //connection.Open();
+            //SqlDataReader reader = command.ExecuteReader();
+            //List<Course> courselist = new List<Course>();
+            //while (reader.Read())
+            //{
+            //    Course course = new Course();
+            //    course.Code = reader["Code"].ToString();
+            //    course.Name = reader["CourseName"].ToString();
+            //    course.SemesterName = reader["SemesterName"].ToString();
+            //    if (!reader["TeacherName"].Equals(System.DBNull.Value))
+            //    {
+            //        course.TeacherName = reader["TeacherName"].ToString();
+            //    }
+            //    else
+            //    {
+            //        course.TeacherName = "Yet not assigned";
+            //    }
 
 
-                courselist.Add(course);
-            }
+            //    courselist.Add(course);
+            //}
 
-            reader.Close();
-            connection.Close();
-            return courselist;
+            //reader.Close();
+            //connection.Close();
+            return null;
         }
         //change by sayed
     }
