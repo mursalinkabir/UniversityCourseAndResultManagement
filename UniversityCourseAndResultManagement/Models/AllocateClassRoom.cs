ce7094d56a6254d68a94a7990edd2369307e91aa
Mursalin Kabir <mail2mursalin@gmail.com>
2016-01-30 07:17:49 +0600

    Allocate Class Save Minor Improvement

--- UniversityCourseAndResultManagement/Models/AllocateClassRoom.cs
+++ UniversityCourseAndResultManagement/Models/AllocateClassRoom.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,15 +9,21 @@ namespace UniversityCourseAndResultManagement.Models
 {
     public class AllocateClassRoom
     {
+        [Required(ErrorMessage = "Please select a Department NUmber")]
         [DisplayName("Department")]
         public int DepartmentId { get; set; }
         [DisplayName("Course")]
+        [Required(ErrorMessage = "Please select a Course")]
         public int CourseId { get; set; }
         [DisplayName("Room")]
+        [Required(ErrorMessage = "Please select a Room")]
         public int RoomId { get; set; }
+        [Required(ErrorMessage = "Please select a Day")]
         public string Day { get; set; }
+         [Required(ErrorMessage = "Please select a start time")]
         [DisplayName("From")]
         public DateTime TimeStart { get; set; }
+         [Required(ErrorMessage = "Please select a end time")]
         [DisplayName("To")]
         public DateTime TimeEnd { get; set; }
     }
